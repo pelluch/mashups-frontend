5825f5d38a20d2c83363ba237a2ceff52b2e0d01
Guillermo Morales <gjmorale@uc.cl>
2014-10-19 20:42:55 -0300

    self

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 require 'active_resource'
 
 class User < ActiveResource::Base
-	self.site = "#"
+	self.site = "http://localhost:3000"
   # self.site = "http://api.people.com:3000"
   def authenticate
   	# Verifica al usuario
