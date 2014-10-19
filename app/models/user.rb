d12820da339d550718333ba78ca17deee7a37826
Guillermo Morales <gjmorale@uc.cl>
2014-10-19 20:59:41 -0300

    Solucionado las vistas de usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ require 'active_resource'
 class User < ActiveResource::Base
 	self.site = "http://localhost:3000"
   # self.site = "http://api.people.com:3000"
+
+  schema do
+  	string 'name', 'password', 'email'
+  end
+
   def authenticate
   	# Verifica al usuario
   end
