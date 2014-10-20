ac1eb0c0052960680853b637c236ef2fda1f1ec2
mperalt3 <mperalt3@uc.cl>
2014-10-19 21:17:10 -0300

    Maca

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 require 'active_resource'
 
 class User < ActiveResource::Base
-	self.site = "#"
-  # self.site = "http://api.people.com:3000"
+	# self.site = "#"
+  	self.site = "http://localhost:3000/"
   def authenticate
   	# Verifica al usuario
   end
