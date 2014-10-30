881cbf9bac3fbc15b7d0602066c9093ca6d3ca8f
Tomas Dussaillant <tpdussai@uc.cl>
2014-10-30 20:38:26 -0300

    user test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	context 'new user' do 
+		let(:user) {User.new}
+
+		its(:name) {should be_nil?}		
+
+	end  
 end
